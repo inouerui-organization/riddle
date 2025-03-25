9e9b0a411e6634ab7bd373a6d7bdc5d3d917deca
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-03-25 15:53:20 +0900

    fix: agent削除

--- src/main/resources/db/migration/V3__create_table_user.sql
+++ src/main/resources/db/migration/V3__create_table_user.sql
@@ -3,6 +3,5 @@ CREATE TABLE users (
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    user_agent VARCHAR(255),
     views_save_time INTEGER
 );
