4c0c86605eb44fcd1209f9f5bb40028b2dff2de2
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-03-25 16:34:26 +0900

    visitor_puzzle_views

---
+++ src/main/resources/db/migration/V8__create_table_visitor_puzzle_view.sql
@@ -0,0 +1,7 @@
+CREATE TABLE visitor_puzzle_views (
+    puzzle_id UUID NOT NULL REFERENCES puzzles(id) ON DELETE CASCADE,
+    visitor_id UUID NOT NULL REFERENCES visitors(id) ON DELETE CASCADE,
+    last_viewed_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (puzzle_id, visitor_id)
+);
