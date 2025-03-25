f3769e1c2c0a35bee2ebeaaf5fe08a2fb050d729
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-03-25 16:31:33 +0900

    user_puzzle_views

---
+++ src/main/resources/db/migration/V7__create_table_user_puzzle_view.sql
@@ -0,0 +1,7 @@
+CREATE TABLE user_puzzle_views (
+    puzzle_id UUID NOT NULL REFERENCES puzzles(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    last_viewed_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (puzzle_id, user_id)
+);
