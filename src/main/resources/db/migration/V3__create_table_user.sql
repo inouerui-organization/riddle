7398379d00a311815ef9401a72d4f7ce041fe089
kaitokosuge <kaitokosuge.mi@gmail.com>
2025-03-26 16:14:07 +0900

    メール認証時間カラム追加

--- src/main/resources/db/migration/V3__create_table_user.sql
+++ src/main/resources/db/migration/V3__create_table_user.sql
@@ -2,6 +2,7 @@ CREATE TABLE app.users (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
+    email_verified_at　TIMESTAMP,
     password VARCHAR(255) NOT NULL,
     views_save_time INTEGER
 );
